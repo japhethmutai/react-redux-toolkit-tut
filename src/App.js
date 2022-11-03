eefae3be1818763cb6dec6949abd6b7561ed2d43
Japheth <japhemchirie@gmail.com>
2022-11-03 12:05:09 +0300

    add posts and users to the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Counter from "./features/counter/Counter";
+import PostsList from "./features/posts/PostsList";
+import AddPostForm from "./features/posts/AddPostForm";
 
 function App() {
 	return (
 		<div className="App">
-			<Counter />
+			<AddPostForm />
+      		<PostsList />
 		</div>
 	);
 }
